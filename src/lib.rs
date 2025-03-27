c6f0e20ad1610a8d5020840fdd13b647b4d66ae9
Say <sailsman@tutanota.com>
2025-03-28 00:19:35 +0100

    processing strings without bind-gen

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-use js_sys::{Array, Map, Reflect};
 use serde::{Deserialize, Deserializer};
-use std::collections::HashMap;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
 
 mod utils;
 
-use wasm_bindgen::prelude::*;
+//use wasm_bindgen::prelude::*;
 
 
+/*
 // todo also consider lines for gate areas
 #[wasm_bindgen]
 pub fn derive(spec: &JsValue, model: &JsValue, state: &JsValue, already_updated_state_this_tick: &JsValue) -> Result<String, JsValue> {
@@ -132,4 +129,56 @@ pub fn derive(spec: &JsValue, model: &JsValue, state: &JsValue, already_updated_
     }
     Err(JsValue::from("What the hell :("))
 }
+*/
+
+/// Allocate memory into the module's linear memory
+/// and return the offset to the start of the block.
+#[no_mangle]
+pub fn alloc(len: usize) -> *mut u8 {
+    // create a new mutable buffer with capacity `len`
+    let mut buf = Vec::with_capacity(len);
+    // take a mutable pointer to the buffer
+    let ptr = buf.as_mut_ptr();
+    // take ownership of the memory block and
+    // ensure that its destructor is not
+    // called when the object goes out of scope
+    // at the end of the function
+    std::mem::forget(buf);
+    // return the pointer so the runtime
+    // can write data at this offset
+    return ptr;
+}
+
+#[no_mangle]
+pub unsafe fn dealloc(ptr: *mut u8, size: usize) {
+    let data = Vec::from_raw_parts(ptr, size, size);
+
+    std::mem::drop(data);
+}
+
+/// Given a pointer to the start of a byte array and
+/// its length, read a string, create its uppercase
+/// representation, then return the pointer to it
+#[no_mangle]
+pub unsafe fn upper(ptr: *mut u8, len: usize) -> *mut u8 {
+    // create a `Vec<u8>` from the pointer and length
+    // here we could also use Rust's excellent FFI
+    // libraries to read a string, but for simplicity,
+    // we are using the same method as for plain byte arrays
+    let data = Vec::from_raw_parts(ptr, len, len);
+    // read a Rust `String` from the byte array,
+    let input_str = String::from_utf8(data).unwrap();
+    // transform the string to uppercase, then turn it into owned bytes
+    let mut upper = input_str.to_ascii_uppercase().as_bytes().to_owned();
+    let ptr = upper.as_mut_ptr();
+    // take ownership of the memory block where the result string
+    // is written and ensure its destructor is not
+    // called whe the object goes out of scope
+    // at the end of the function
+    std::mem::forget(upper);
+    // return the pointer to the uppercase string
+    // so the runtime can read data from this offset
+    ptr
+}
+
 
--- www/index.js
+++ www/index.js
@@ -1,5 +1,3 @@
-import * as wasm from "sl-derive";
-
 function replacer(key, value) {
     if(value instanceof Map) {
         return {
@@ -35,4 +33,70 @@ const alreadyUpdatedState = {
     }
 }
 
-console.log(wasm.derive(spec, model, state, alreadyUpdatedState));
+//console.log(wasm.derive(spec, model, state, alreadyUpdatedState));
+
+const base64 = "";
+
+// Copy `data` into the `instance` exported memory buffer.
+function copyMemory(data, instance) {
+    // the `alloc` function returns an offset in
+    // the module's memory to the start of the block
+    var ptr = instance.exports.alloc(data.length);
+    // create a typed `ArrayBuffer` at `ptr` of proper size
+    var mem = new Uint8Array(instance.exports.memory.buffer, ptr, data.length);
+    // copy the content of `data` into the memory buffer
+    mem.set(new Uint8Array(data));
+    // return the pointer
+    return ptr;
+}
+
+// Read a string from the instance's memory.
+function readString(ptr, len, instance) {
+    var m = new Uint8Array(instance.exports.memory.buffer, ptr, len);
+    var decoder = new TextDecoder("utf-8");
+    // return a slice of size `len` from the module's
+    // memory, starting at offset `ptr`
+    return decoder.decode(m.slice(0, len));
+}
+
+function deallocGuestMemory(ptr, len, instance) {
+    // call the module's `dealloc` function
+    instance.exports.dealloc(ptr, len);
+}
+
+// Invoke the `upper` function from the module
+// and log the result to the console.
+function upper(input, instance) {
+    // transform the input string into its UTF-8
+    // representation
+    var bytes = new TextEncoder("utf-8").encode(input);
+    // copy the contents of the string into
+    // the module's memory
+    var ptr = copyMemory(bytes, instance);
+    // call the module's `upper` function and
+    // get the offset into the memory where the
+    // module wrote the result string
+    var res_ptr = instance.exports.upper(ptr, bytes.length);
+    // read the string from the module's memory,
+    // store it, and log it to the console
+    var result = readString(res_ptr, bytes.length, instance);
+    console.log(result);
+    // the JavaScript runtime took ownership of the
+    // data returned by the module, which did not
+    // deallocate it - so we need to clean it up
+    deallocGuestMemory(res_ptr, bytes.length, instance);
+}
+
+
+(async () => {
+    const binaryString = atob(base64);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+
+    const mod = new WebAssembly.Module(bytes);
+    const instance = await WebAssembly.instantiate(mod, {});
+
+    upper("this should be uppercase", instance);
+})();
