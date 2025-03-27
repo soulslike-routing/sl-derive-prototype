b3bf28d9f8a356f4bc5cf5ce64af17a9d6995edc
Say <sailsman@tutanota.com>
2025-03-27 15:57:49 +0100

    multiple nested objects into wasm

--- src/lib.rs
+++ src/lib.rs
@@ -17,20 +17,35 @@ pub fn greet() {
 }
 
 #[wasm_bindgen]
-pub fn process_nested_js_object(obj: &JsValue) -> Result<String, JsValue> {
+pub fn process_nested_js_object(obj0: &JsValue, obj1: &JsValue) -> Result<String, JsValue> {
     // Get the "user" object
-    let user = Reflect::get(obj, &JsValue::from_str("user"))?;
+    let user0 = Reflect::get(obj0, &JsValue::from_str("user"))?;
 
     // Extract "name" from user
-    let name = Reflect::get(&user, &JsValue::from_str("name"))?
+    let name0 = Reflect::get(&user0, &JsValue::from_str("name"))?
         .as_string()
         .ok_or_else(|| JsValue::from_str("Missing 'name' field"))?;
 
     // Extract "age" from user
-    let age = Reflect::get(&user, &JsValue::from_str("age"))?
+    let age0 = Reflect::get(&user0, &JsValue::from_str("age"))?
         .as_f64()
         .ok_or_else(|| JsValue::from_str("Missing 'age' field"))?;
 
-    Ok(format!("User: {}, Age: {}", name, age))
+    // Get the "user" object
+    let user1 = Reflect::get(obj1, &JsValue::from_str("user"))?;
+    // Get the "nested" object
+    let nested1 = Reflect::get(&user1, &JsValue::from_str("nested"))?;
+
+    // Extract "name" from user
+    let name1 = Reflect::get(&nested1, &JsValue::from_str("name"))?
+        .as_string()
+        .ok_or_else(|| JsValue::from_str("Missing 'name' field"))?;
+
+    // Extract "age" from user
+    let age1 = Reflect::get(&nested1, &JsValue::from_str("age"))?
+        .as_f64()
+        .ok_or_else(|| JsValue::from_str("Missing 'age' field"))?;
+
+    Ok(format!("User: {}, Age: {}, User: {}, Age: {}", name0, age0, name1, age1))
 }
 
--- www/index.js
+++ www/index.js
@@ -3,11 +3,20 @@ import * as wasm from "sl-derive";
 wasm.greet();
 
 
-const obj = {
+const obj0 = {
     user: {
-        name: "testing",
+        name: "userA",
         age: 30
     }
 };
 
-console.log(wasm.process_nested_js_object(obj)); // "User: Alice, Age: 30"
+const obj1 = {
+    user: {
+        nested: {
+            name: "nested",
+            age: 30
+        }
+    }
+};
+
+console.log(wasm.process_nested_js_object(obj0, obj1)); // "User: Alice, Age: 30"
